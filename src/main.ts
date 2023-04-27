d8c1a634f6bf6f5f63906d8984d8ccfcb45db126
Marko Skugor <marko.skugor.1@gmail.com>
2023-04-27 16:47:37 -0400

    demo code

--- src/main.ts
+++ src/main.ts
@@ -1,61 +1,7 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { DocumentResizeMessageService } from './document-resize-message.service';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  styles: [
-    `
-    :host {
-      background: palegoldenrod;
-      display: block;
-      padding: 1rem;
-    }
+import { AppModule } from './app/app.module';
 
-    .button-container {
-      display: flex;
-      flex-wrap: wrap;
-      gap: 1rem;
-    }
-  `,
-  ],
-  imports: [CommonModule],
-  template: `
-    <div class="button-container">
-      <button (click)="increaseHeight()">Increase Height</button>
-      <button (click)="decreaseHeight()">Decrease Height</button>
-      <button (click)="increaseWidth()">Increase Width</button>
-      <button (click)="decreaseWidth()">Decrease Width</button>
-    </div>
-    <div [ngStyle]="{ 'height.px': height, 'width.px': width }"></div>
-  `,
-})
-export class App {
-  height = 0;
-  width = 0;
 
-  constructor(documentResizeMessageService: DocumentResizeMessageService) {
-    documentResizeMessageService.init();
-  }
-
-  increaseHeight() {
-    this.height = this.height + 50;
-  }
-
-  decreaseHeight() {
-    this.height = this.height === 0 ? 0 : this.height - 50;
-  }
-
-  increaseWidth() {
-    this.width = this.width + 50;
-  }
-
-  decreaseWidth() {
-    this.width = this.width === 0 ? 0 : this.width - 50;
-  }
-}
-
-bootstrapApplication(App);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
